ff7f01a2680db7aa314cb0fd8c9e6fda4511ae0e
Sean Morgan <insideseanshead@gmail.com>
2020-11-05 12:29:18 -0800

    added pages and routes

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Fluid jumbotron</h1>
+          <p className="lead">
+            This is a modified jumbotron that occupies the entire horizontal
+            space of its parent.
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+            <div className="col">
+                <h1>Welcome to Pupster</h1>
+                <p>
+                Puppy kitty ipsum dolor sit good dog foot stick canary. Teeth Mittens grooming vaccine walk swimming nest good boy furry tongue heel furry treats fish. Cage run fast kitten dinnertime ball run foot park fleas throw house train licks stick dinnertime window. Yawn litter fish yawn toy pet gate throw Buddy kitty wag tail ball groom crate ferret heel wet nose Rover toys pet supplies. Bird Food treats tongue lick teeth ferret litter box slobbery litter box crate bird small animals yawn small animals shake slobber gimme five toys polydactyl meow. Turtle cage lazy cat foot lazy cat groom canary window tooth brush bedding lazy cat pet supplies turtle water dog shake pet supplies kitty. Walk bird harness wet nose meow harness grooming water dog lol catz water bedding toys bird seed fetch lazy cat. Parakeet scratcher brush biscuit lick dog tooth walk food lazy cat biscuit. Cockatiel Snowball kitten Rover ferret puppy.
+                </p>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
